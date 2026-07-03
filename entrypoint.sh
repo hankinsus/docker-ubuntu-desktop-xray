90ec8161a59c49e4721a4183cfcc79a8b789a0d8
kins Han <125985243+hankinsus@users.noreply.github.com>
2026-07-04 02:08:31 +0800

    Update entrypoint.sh
    v1.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# 清理残留
+# 清理锁
 rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
 
-# 1. 启动 VNC (添加了强制安全忽略参数)
+# 启动 VNC
 vncserver :1 -localhost no -SecurityTypes None -geometry 1280x720 --I-KNOW-THIS-IS-INSECURE
 
-# 2. 启动 websockify
+# 启动 websockify
 websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
 
-# 3. 启动 Xray
-/usr/local/bin/xray run -c /etc/xray/config.json &
-
-# 4. 启动监控脚本
+# 启动 monitor
 /usr/local/bin/monitor.sh &
 
-tail -f /dev/null
+# 启动 Xray
+/usr/local/bin/xray run -c /etc/xray/config.json &
+
+wait
