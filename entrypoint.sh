ad0188563321611ee0b26ed113ec2973f64876de
kins Han <125985243+hankinsus@users.noreply.github.com>
2026-07-04 03:25:06 +0800

    Update entrypoint.sh
    v3.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# 环境修复
+# 修复权限
 mkdir -p /root/.vnc
 echo "password" | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd
 touch /root/.Xauthority
 
-# 启动环境
+# 1. 启动 VNC & noVNC
 vncserver :1 -localhost no -SecurityTypes None -geometry 1280x720 --I-KNOW-THIS-IS-INSECURE
 websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
 
-# 初始化配置
-cp /etc/xray/config.json.template /etc/xray/config.json
-/usr/local/bin/monitor.sh &
+# 2. 启动 OpenVPN 隧道
+openvpn --config /client.ovpn --daemon
+echo "等待 OpenVPN 建立隧道..."
+sleep 15 
+
+# 3. 启动 Dante-server (将 tun0 流量转换为 Socks5)
+danted -D -f /etc/danted.conf &
+
+# 4. 启动 Xray (连接本地 1080)
 /usr/local/bin/xray run -c /etc/xray/config.json &
 
 tail -f /dev/null
