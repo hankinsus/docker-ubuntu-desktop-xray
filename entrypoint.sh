a930faff8176215d8ab8dc965533811ab49594f8
kins Han <125985243+hankinsus@users.noreply.github.com>
2026-07-04 02:42:23 +0800

    Update entrypoint.sh
    v1.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-# 清理锁
-rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
-
-# 启动 VNC
+rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1 /root/.Xauthority
 vncserver :1 -localhost no -SecurityTypes None -geometry 1280x720 --I-KNOW-THIS-IS-INSECURE
-
-# 启动 websockify
 websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
-
-# 启动 monitor
 /usr/local/bin/monitor.sh &
-
-# 启动 Xray
 /usr/local/bin/xray run -c /etc/xray/config.json &
-
-wait
+tail -f /dev/null
