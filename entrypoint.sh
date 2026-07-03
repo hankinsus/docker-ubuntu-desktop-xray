f059cdf3118315e6719c479de7f7109897435c35
kins Han <125985243+hankinsus@users.noreply.github.com>
2026-07-04 03:02:58 +0800

    Update entrypoint.sh
    v2.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1 /root/.Xauthority
+# 修复 VNC 权限
+mkdir -p /root/.vnc
+echo "password" | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
+touch /root/.Xauthority
+
+# 启动环境
 vncserver :1 -localhost no -SecurityTypes None -geometry 1280x720 --I-KNOW-THIS-IS-INSECURE
 websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
-/usr/local/bin/monitor.sh &
+
+# 自动处理配置：如果不存在 config.json，则复制模板
+if [ ! -f /etc/xray/config.json ]; then
+    cp /etc/xray/config.json.template /etc/xray/config.json
+fi
+
+# 启动 Xray
 /usr/local/bin/xray run -c /etc/xray/config.json &
+
 tail -f /dev/null
